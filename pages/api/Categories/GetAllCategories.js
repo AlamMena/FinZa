c0f672445818d4ae880d740fd0142a001d5b78c8
Alam Mena <extamena@asociacionlanacional.com.do>
2022-08-19 17:14:42 -0400

    api

--- pages/api/Categories/GetAllCategories.js
+++ pages/api/Categories/GetAllCategories.js
@@ -5,10 +5,10 @@ import { client, db } from "../Database/Database";
 export default async function handler(req, res) {
   try {
     await client.connect();
-    const categories = await db.collection("categories").find({}).toArray();
+    const categories = await db.collection("categories").findOne({});
     await client.close();
     return res.status(201).json(categories);
   } catch (error) {
-    res.status(500).json({ message: "An error has occurred" });
+    res.status(500).json({ message: "An error has occurred",error });
   }
 }
