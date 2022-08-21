67f74bd6badc3e11f2ea41184855c781dfa35b27
Alam <amenabeato@gmail.com>
2022-08-21 01:30:40 -0400

    card color fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-import SideBar from '../Components/SideBar';
-import '../styles/globals.css'
-
+import SideBar from "../Components/SideBar";
 function MyApp({ Component, pageProps }) {
   return (
-  <div className='flex w-full '>
-    <SideBar/>
-    <div className='lg:ml-64 md:ml-64 px-8 md:px-10 py-8 w-full'>
-    <Component {...pageProps} />
+    <div className="flex w-full ">
+      <SideBar />
+      <div className="lg:ml-64 md:ml-64 px-8 md:px-10 py-8 w-full">
+        <Component {...pageProps} />
+      </div>
     </div>
-    </div>);
+  );
 }
 
-export default MyApp
+export default MyApp;
