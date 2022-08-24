7e09ca29b4b30544bcfe00cbb906919450ae8c4d
Alam <amenabeato@gmail.com>
2022-08-23 23:47:59 -0400

    global grid design made

--- Components/CategoriesList.js
+++ Components/CategoriesList.js
@@ -1,35 +1,39 @@
 import { Alert, Button, Snackbar } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, gridClasses } from "@mui/x-data-grid";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteCategory } from "../Store/categorySlice";
 import ConfirmDialog from "./ConfirmDialog";
+
 export default function CategoriesList({ setFormOpen, setFormData }) {
   // confirm dialog state
   const [confirmOpen, setConfirmOpen] = useState(false);
   const [itemToDelete, setItemToDelete] = useState();
 
+  // alert states
   const [alert, setAlert] = useState(false);
   const [alertParams, setAlertParams] = useState({
     severity: "success",
     title: "",
   });
-
-  const dispatch = useDispatch();
   const columns = [
     {
-      field: "id",
-      width: 180,
-      headerName: "Id",
-    },
-    {
-      field: "name",
-      width: 200,
+      fiels: "Image",
+      width: "auto",
       headerName: "Name",
       renderCell: (cells) => {
-        return <a>{cells.row.name}</a>;
+        return (
+          <div className="flex space-x-4 items-center">
+            <img
+              className=" rounded-xl w-10 h-10"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXQw_lYmrP6EnN8mHsBmJfrP4e6E9JW6An2A&usqp=CAU"
+            />
+            <span className="font-semibold">{cells.row.name}</span>
+          </div>
+        );
       },
     },
+
     {
       field: "Actions",
       sortable: false,
@@ -58,9 +62,33 @@ export default function CategoriesList({ setFormOpen, setFormData }) {
         );
       },
     },
+    {
+      field: "id",
+      width: 180,
+      headerName: "Id",
+    },
   ];
 
+  const dispatch = useDispatch();
   const categories = useSelector((state) => state.category);
+
+  const handleConfirmForm = () => {
+    setConfirmOpen(false);
+    try {
+      dispatch(deleteCategory(itemToDelete));
+      setAlertParams({
+        severity: "success",
+        title: "category deleted successfully",
+      });
+      setAlert(true);
+    } catch (error) {
+      setAlertParams({
+        severity: "error",
+        title: "Oops!, something went wrong, try later",
+      });
+      setAlert(true);
+    }
+  };
 
   return (
     <div className="w-full h-96">
@@ -77,35 +105,31 @@ export default function CategoriesList({ setFormOpen, setFormData }) {
         </Button>
       </div>
 
-      <DataGrid
-        rows={categories.data}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        loading={false}
-      />
+      <div className="w-full h-full shadow-lg px-4 py-2 rounded-xl">
+        <DataGrid
+          GridLinesVisibility="None"
+          rows={categories.data}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          loading={false}
+          className="white"
+          // sx={{
+          //   boxShadow: 2,
+          //   borderRadius: 4,
+          //   borderCollapse: "none",
+          //   padding: "20px",
+          //   "& .MuiDataGrid-cell:hover": {
+          //     color: "primary.main",
+          //   },
+          // }}
+        />
+      </div>
       <ConfirmDialog
-        title="are u sure?"
+        title="Are you sure?"
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
-        onConfirm={() => {
-          setConfirmOpen(false);
-          try {
-            dispatch(deleteCategory(itemToDelete));
-            setAlertParams({
-              severity: "success",
-              title: "category deleted successfully",
-            });
-            setAlert(true);
-            throw new Excetion();
-          } catch (error) {
-            setAlertParams({
-              severity: "error",
-              title: "Oops!, something went wrong, try later",
-            });
-            setAlert(true);
-          }
-        }}
+        onConfirm={handleConfirmForm}
       />
       <Snackbar
         open={alert}
