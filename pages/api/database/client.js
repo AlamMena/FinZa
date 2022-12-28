343afaca467243828e8f5bde5eba7bd5690a3b32
Alam2701 <amenabeato@gmail.com>
2022-12-28 16:55:21 -0400

    data

--- pages/api/database/client.js
+++ pages/api/database/client.js
@@ -1,4 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = "mongodb://localhost:27017";
-// "mongodb+srv://Alam:Alam2701@cluster0.cf05i1x.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb://localhost:27017";
+// ;
+const uri =
+  "mongodb+srv://Alam:Alam2701@cluster0.cf05i1x.mongodb.net/?retryWrites=true&w=majority";
+
 export default client = new MongoClient(uri);
