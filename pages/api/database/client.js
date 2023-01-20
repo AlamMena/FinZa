76495e22a40d4bf55bfae4d6e90718ad47b4e9b6
Alam2701 <amenabeato@gmail.com>
2023-01-20 08:55:40 -0400

    updates

--- pages/api/database/client.js
+++ pages/api/database/client.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 // const uri = "mongodb://localhost:27017";
 const uri =
-  "mongodb+srv://Alam:Alam2701@cluster0.cf05i1x.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Finza:Alam2701@finza.xnlmjkn.mongodb.net/?retryWrites=true&w=majority";
 
 let clientPromise;
 
